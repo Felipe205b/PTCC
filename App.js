e4cd9b2be9b8b17ddec57216fe438671eac8c206
Felipe205b <felipesousadacosta@gmail.com>
2023-06-01 11:30:43 -0300

    teste

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import C1 from './components/comp1';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <C1/>
+      <C1/>
+      <Text style={estilos.txt1}> STEIN</Text>
     </View>
-  );
-}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};//fechamento do class App
+
+  const estilos= StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor: '#435570',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+    txt1:{
+      color: '#4287f5',
+      fontSize: 48,
+      
+    }
+
+
+  });
---
+++ components/comp1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function(){
+    return(
+        <Text style={estilos.txt1}>Stein</Text>
+    )
+}
+const estilos= StyleSheet.create({
+    txt1:{
+      color: '#b048',
+      fontSize: 48,
+      
+    }
+
+
+  });
+
